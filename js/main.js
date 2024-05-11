0f27d292f05fe25a4e3f51345f99a2b8f8ea2c67
Benjamin Eceiza <benjaceiza400@gmail.com>
2024-05-10 23:47:49 -0300

    hasbro

--- js/main.js
+++ js/main.js
@@ -1,106 +1,231 @@
+let arrayDeProductos;
+
+if (localStorage.getItem("arrayDeProductos")){
+    arrayDeProductos = JSON.parse(localStorage.getItem("arrayDeProductos"));
+    mostrar(arrayDeProductos);
+  
+ 
+}else{
+
+    arrayDeProductos = [
+        {
+            nombre:"Monopoly",
+            precio:1000,
+            stock:50,
+            imagen:"../recursos/img-producto/monopoly.webp",
+        },
+        {
+            nombre:"Baby Alive",
+            precio:3000,
+            stock:20,
+            imagen:"../recursos/img-producto/baby.webp",
+        },
+        {
+            nombre:"Transformer",
+            precio:4500,
+            stock:20,
+            imagen:"../recursos/img-producto/transformer.webp",
+        },
+        {
+            nombre:"Operando",
+            precio:1000,
+            stock:50,
+            imagen:"../recursos/img-producto/operando.webp",
+        },
+    ];
+    localStorage.setItem("arrayDeProductos",JSON.stringify(arrayDeProductos));
+    mostrar(arrayDeProductos);
+   
+}
+
+
+
+
+
 class Creador {
-    constructor(nombre, precio, stock, id) {
+    constructor(nombre, precio, stock, imagen) {
         this.nombre = nombre;
         this.precio = precio;
-        while (this.precio <= 0) {
-            this.precio = prompt("Ingrese un precio positivo");
-        }
         this.stock = stock;
-        while (this.stock <= 0) {
-            this.stock = prompt("Ingrese un stock positivo");
-        }
-        this.id = id;
+        this.imagen = imagen;
+
+        $inputNombre.value = "";
+        $inputPrecio.value = "";
+        $inputStock.value = "";
+        $inputImagen.value = "";
     }
 }
 
-function cargadoConExito(array) {
 
-    alert("Producto: " + array.nombre + "\nCARGADO EXITOSAMENTE!");
+
+
+
+
+
+function control(nombre, precio, stock, imagen) {
+    if (nombre == "") {
+        alert("Ingrese un nombre")
+    } else if (imagen == "") {
+        alert("Ingrese una imagen")
+    } else if (precio <= 0 && stock <= 0) {
+        alert("ingrese un numero positivo");
+        $inputPrecio.value = "";
+        $inputStock.value = "";
+    } else if (precio <= 0 || stock <= 0) {
+        alert("inrese un numero positivo");
+        $inputPrecio.value = "";
+        $inputStock.value = "";
+    } else if (isNaN(precio) && isNaN(stock)) {
+        alert("Ingrese precio y stock con números");
+        $inputPrecio.value = "";
+        $inputStock.value = "";
+    } else if (isNaN(precio) || isNaN(stock)) {
+        alert("Ingrese precio y stock con números");
+        $inputPrecio.value = "";
+        $inputStock.value = "";
+    } else {
+       
+        alert("Producto cargado con exito!");
+        cargar($inputNombre.value, $inputPrecio.value,$inputStock.value,$inputImagen.value);
+        
+    }
+
+}
+
+function cargar(nombre, precio, stock,imagen) {
+    const nuevoProducto = new Creador(nombre, precio, stock,imagen);
+    arrayDeProductos.unshift(nuevoProducto);
+    localStorage.setItem("arrayDeProductos",JSON.stringify(arrayDeProductos));
+    mostrar(arrayDeProductos);
 }
 
 
-let opcion;
-let busqueda;
-const arrayDeProductos = [
-    {
-        id: 1,
-        nombre: "foco",
-        precio: 500,
-        stock: 200,
-    },
-    {
-        id: 2,
-        nombre: "pelota",
-        precio: "1000",
-        stock: 50,
-    },
-    {
-        id: 3,
-        nombre: "teclado",
-        precio: 1500,
-        stock: 20,
-    }
-];
+function mostrar(arrayDeProductos) {
+    const conteiner = document.getElementById("conteiner-productos");
+    conteiner.innerText = "";
+    arrayDeProductos.forEach(el => {
+        const card = document.createElement("div");
+        card.className = "productos__card";
+        const nombre = document.createElement("p");
+        nombre.innerText = el.nombre;
+        nombre.className = "productos__nombre"
+        const imagen = document.createElement("img");
+        imagen.src = el.imagen;
+        imagen.className = "productos__img"
+        const conteinerPrecioStock = document.createElement("div");
+        conteinerPrecioStock.className = "productos__stock-container";
+        const precio = document.createElement("p");
+        precio.className = "productos__precio";
+        precio.innerText = "$" + el.precio;
+        const stock = document.createElement("input");
+        stock.className = "productos__stock";
+        stock.placeholder= "1";
+        stock.type = "number"
+        const $botonCarrito = document.createElement("button");
+        $botonCarrito.className = "productos__boton"
+        $botonCarrito.innerText = "Enviar a Carrito"
+        
+        card.appendChild(nombre);
+        card.appendChild(imagen);
+        conteinerPrecioStock.appendChild(precio);
+        conteinerPrecioStock.appendChild(stock);
+        conteinerPrecioStock.appendChild($botonCarrito)
+        card.appendChild(conteinerPrecioStock);
+        conteiner.appendChild(card);
 
-do {
-    opcion = parseInt(prompt("Ingrese una opcion\n\n 1. Cargar Producto Nuevo\n 2. Ver productos\n 3. Buscar pruductos\n 4. Publicar productos\n 0. salir"));
+    })
+}
+
+
+function buscarPorNombre(nombre){
+    const nuevoArrayDeProductos = arrayDeProductos.filter(el => el.nombre !== nombre);
+    arrayDeProductos = nuevoArrayDeProductos;
+    localStorage.setItem("arrayDeProductos",JSON.stringify(arrayDeProductos));
+}
+
+function mostrarFormulario(opcion) {
+
     switch (opcion) {
         case 1:
-            const nuevoProducto = new Creador(prompt("Ingrese nombre del producto"),
-                parseInt(prompt("Ingrese precio del producto")),
-                parseInt(prompt("Ingrese el stock del producto")),
-                parseInt(prompt("Ingrese el id del producto")));
-            arrayDeProductos.unshift(nuevoProducto);
-            cargadoConExito(nuevoProducto);
+          
+            $conteinerFormulario.innerText = "";
+            $inputNombre.value = "";
+            $inputPrecio.value = "";
+            $inputStock.value = "";
+            $inputImagen.value = "";
+            $conteinerFormulario.appendChild($inputNombre);
+            $conteinerFormulario.appendChild($inputPrecio);
+            $conteinerFormulario.appendChild($inputStock);
+            $conteinerFormulario.appendChild($inputImagen);
+            $conteinerFormulario.appendChild($botonEnviar);
+
+            $botonEnviar.onclick = () => control($inputNombre.value, parseInt($inputPrecio.value), $inputStock.value, $inputImagen.value);
             break;
         case 2:
-            arrayDeProductos.forEach(el => alert("Id: " + el.id + "\nNombre del producto: " + el.nombre + "\nprecio: " + el.precio + "\nstock: " + el.stock))
+            $conteinerFormulario.innerText = "";
+            $inputNombre.value = "";
+            $conteinerFormulario.appendChild($inputNombre);
+            $conteinerFormulario.appendChild($botonEnviar);
+            $botonEnviar.onclick = () => buscarPorNombre($inputNombre.value);
+            console.log($inputNombre.value);
             break;
         case 3:
-            busqueda = prompt("Ingrese el nombre del producto a buscar");
-            let encontrado = arrayDeProductos.find(el => el.nombre === busqueda);
-            if (encontrado == undefined) {
-                alert("ese producto no existe");
-            } else {
-                alert("Producto encontrado : \n\nId: " + encontrado.id + "\nNombre: " + encontrado.nombre + "\nPrecio: " + encontrado.precio)
-            }
-            break;
-        case 4:
-            for (let i = 0; i < arrayDeProductos.length; i++) {
-                const conteiner = document.getElementById("conteiner-productos");
-                const card = document.createElement("div");
-                card.className = "card";
-                const nombre = document.createElement("p");
-                nombre.innerText = arrayDeProductos[i].nombre;
-                nombre.className = "nombre-producto"
-                const imagen = document.createElement("img");
-                imagen.src = "https://static.vecteezy.com/system/resources/previews/004/141/669/non_2x/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg"
-                imagen.className = "imagen-producto"
-                const conteinerPrecioStock = document.createElement("div");
-                conteinerPrecioStock.className = "stock-precio";
-                const precio = document.createElement("p");
-                precio.className ="precio";
-                precio.innerText = "$" +arrayDeProductos[i].precio;
-                const stock = document.createElement("p");
-                stock.className = "stock";
-                stock.innerText = arrayDeProductos[i].stock + " U";
-
-                card.appendChild(nombre);
-                card.appendChild(imagen);
-                card.appendChild(conteinerPrecioStock);
-                conteinerPrecioStock.appendChild(precio);
-                conteinerPrecioStock.appendChild(stock);
-                conteiner.appendChild(card);
-            }
-            opcion = 0;
-            break;
-        case 0:
-            alert("hasta luego");
+            $conteinerFormulario.innerText = "";
+            $inputNombre.value = "";
+            $inputPrecio.value = "";
+            $inputStock.value = "";
+            $inputImagen.value = "";
+            $conteinerFormulario.appendChild($inputNombre);
+            $conteinerFormulario.appendChild($inputPrecio);
+            $conteinerFormulario.appendChild($inputStock);
+            $conteinerFormulario.appendChild($inputImagen);
+            $conteinerFormulario.appendChild($botonEnviar);
             break;
         default:
-            alert("Opcion invalida");
             break;
     }
-} while (opcion !== 0);
+}
+
+
+const $botonAgregar = document.getElementById("boton-agregar");
+const $botonEliminar = document.getElementById("boton-eliminar");
+const $botonEditar = document.getElementById("boton-editar");
+
+const $conteinerFormulario = document.getElementById("conteiner-formulario")
+const $inputNombre = document.createElement("input");
+$inputNombre.placeholder = "Nombre Producto";
+$inputNombre.className = "input-formulario";
+
+const $inputPrecio = document.createElement("input");
+$inputPrecio.placeholder = "Precio Producto";
+$inputPrecio.className = "input-formulario";
+
+const $inputStock = document.createElement("input");
+$inputStock.placeholder = "Stock De Producto";
+$inputStock.type = "number";
+$inputStock.className = "input-formulario";
+
+const $inputImagen = document.createElement("input");
+$inputImagen.placeholder = "Dirección de imagen";
+$inputImagen.className = "input-formulario";
+
+const $botonEnviar = document.createElement("button");
+$botonEnviar.innerText = "Enviar";
+$botonEnviar.className = "boton-administrador"
+
+$botonAgregar.onclick = () => mostrarFormulario(1)
+$botonEliminar.onclick = () => mostrarFormulario(2)
+$botonEditar.onclick = () => mostrarFormulario(3)
+
+
+
+
+
+
+
+
+
+
+
 
 
